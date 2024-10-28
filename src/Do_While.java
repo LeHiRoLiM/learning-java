fa6aa2b5ed7d67abec67e1a1abdc1c486849c101
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-10-28 17:23:30 -0300

    new estructure

--- src/Do_While.java
+++ src/Do_While.java
@@ -1,10 +1,20 @@
-//public class Do_While
-//{
-//    public static void main(String[] args)
-//    {
-//        int jira = 0;
-//        do {
-//            System.out.println("");
-//        }
-//    }
-//}
+import java.util.Scanner;
+
+public class Do_While
+{
+    public static void main(String[] args)
+    {
+        int num, soma = 0;
+        String resposta;
+        Scanner teclado = new Scanner(System.in);
+        do {
+            System.out.println("Digite um número: ");
+            num = teclado.nextInt();
+            soma += num;
+            System.out.println("Quer continuar ? [S/N]");
+            resposta = teclado.next();
+        } while (resposta.equals("S")) ;
+        System.out.println("A soma de todos os valores é " + soma);
+
+    }
+}
