f50028318e22c22ec718aa1a7e8e729289c019a0
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-10-28 16:50:01 -0300

    corrigindo WHile_Repetir

---
+++ src/Do_While.java
@@ -0,0 +1,10 @@
+//public class Do_While
+//{
+//    public static void main(String[] args)
+//    {
+//        int jira = 0;
+//        do {
+//            System.out.println("");
+//        }
+//    }
+//}
--- src/While_Repetir.java
+++ src/While_Repetir.java
@@ -7,10 +7,21 @@ public class While_Repetir
             jira--;
             if (jira >1) {
         }
-            System.out.println( jira + " patinhos foram passear\n" +
-                    "Além das montanhas\n" +
-                    "Para brincar\n" +
-                    "A mamãe gritou: Quá, quá, quá, Mas só " + ( jira - 1) + " patinhos voltaram de lá.\n");
+            if (jira >=3)
+            {
+                System.out.println(jira + " patinhos foram passear\n" +
+                        "Além das montanhas\n" +
+                        "Para brincar\n" +
+                        "A mamãe gritou: Quá, quá, quá, Mas só " + (jira - 1) + " patinhos voltaram de lá.\n");
+            }else
+            {
+                System.out.println(jira + " patinho foi passear\n" +
+                        "Além das montanhas\n" +
+                        "Para brincar\n" +
+                        "A mamãe gritou: Quá, quá, quá, Mas só " + (jira - 1) + " patinho voltou de lá.\n");
+            }
+
+
         }
     }
 }
