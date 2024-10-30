5b72e3f559cd6f544363be8c16acbd036898e762
Lehi - ASUS Vivo <lehirolim@gmail.com>
2024-10-30 15:45:07 -0300

    new stuff

---
+++ src/Metodos.java
@@ -0,0 +1,12 @@
+public class Metodos
+{
+        static void soma( int a, int b)
+        {
+            int c = a + b;
+            System.out.println("A soma é " + c);
+        }
+        public static void main(String[] args)
+        {
+            soma(5,2);
+        }
+}
